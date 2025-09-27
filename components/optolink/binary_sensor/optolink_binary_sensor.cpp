227c74d937f41a37fa175ef10013749057d894c1
Vladimir Shuklin <vladimir@shuklin.info>
2025-09-27 17:20:18 +0300

    add logs

--- components/optolink/binary_sensor/optolink_binary_sensor.cpp
+++ components/optolink/binary_sensor/optolink_binary_sensor.cpp
@@ -3,18 +3,25 @@
 #include "optolink_binary_sensor.h"
 #include "../optolink.h"
 
-namespace esphome {
-namespace optolink {
+namespace esphome
+{
+  namespace optolink
+  {
+    static const char *const TAG = "optolink.binary_sensor";
 
-static const char *const TAG = "optolink.binary_sensor";
-
-void OptolinkBinarySensor::datapoint_value_changed(uint8_t value) {
-  if (value != 5) {
-    publish_state((int8_t) value);
-  }
-};
-
-}  // namespace optolink
-}  // namespace esphome
+    void OptolinkBinarySensor::datapoint_value_changed(uint8_t value)
+    {
+      if (value != 5)
+      {
+        publish_state((int8_t)value);
+      }
+      else
+      {
+        ESP_LOGD(TAG, "ignoring value %s for binary sensor",
+                 value().c_str());
+      }
+    };
+  } // namespace optolink
+} // namespace esphome
 
 #endif
